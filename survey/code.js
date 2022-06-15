00c9176aab9b04a5a9ac5d526e456eea2289ff75
HabibKhan1 <linuxforlife1@outlook.com>
2022-06-15 16:00:53 -0500

    Update code.js

--- survey/code.js
+++ survey/code.js
@@ -1,29 +1,7 @@
 codes = new Set();
 var d = Date.now();
 
-codes.new("JNXCB-JWNZF");
-
-function code ()
-{
-	c = document.getElementById("code_input");
-
-	if (codes.has(c.value) == true && 1000 * 60 * 5 + d < Date.now())
-	{
-		alert("Thank you!")
-
-		document.write("<!DOCTYPE HTML>");
-		document.write("<html>");
-		document.write("<head>");
-		document.write("</head>");
-		document.write("<body>");
-		document.write("<p>Please print out this page for Habib Khan to approve.</p>");
-		document.write("<button class = 'button' onclick = 'print()'><p>Print</p></button");
-		document.write("</body>");
-		document.write("</html>");
-	}
-}
-
-codes.add("TRBYM-TJTRZ");
+codes.add("EUDEP-POZQW");
 
 function code ()
 {
