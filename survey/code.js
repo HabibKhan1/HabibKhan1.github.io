c665976cb6c923d6fea7afea9f9ca39e25e166ad
HabibKhan1 <linuxforlife1@outlook.com>
2022-06-15 15:42:06 -0500

    Update code.js

--- survey/code.js
+++ survey/code.js
@@ -1,15 +1,15 @@
 codes = new Set();
 var d = Date.now();
 
-// SEEK
+codes.new("PWNPG-XZGVR");
 
 function code ()
 {
-	var c = document.getElementById("code_input");
+	c = document.getElementById("code_input");
 
 	if (codes.has(c.value) == true && 1000 * 60 * 5 + d < Date.now())
 	{
-		alert("Thank you!");
+		alert("Thank you!")
 
 		document.write("<!DOCTYPE HTML>");
 		document.write("<html>");
@@ -18,6 +18,205 @@ function code ()
 		document.write("<body>");
 		document.write("<p>Please print out this page for Habib Khan to approve.</p>");
 		document.write("<button class = 'button' onclick = 'print()'><p>Print</p></button");
+		document.write("</body>");
 		document.write("</html>");
 	}
-} 
+}
+
+codes.new("GQLCY-XVKQE");
+
+function code ()
+{
+	c = document.getElementById("code_input");
+
+	if (codes.has(c.value) == true && 1000 * 60 * 5 + d < Date.now())
+	{
+		alert("Thank you!")
+
+		document.write("<!DOCTYPE HTML>");
+		document.write("<html>");
+		document.write("<head>");
+		document.write("</head>");
+		document.write("<body>");
+		document.write("<p>Please print out this page for Habib Khan to approve.</p>");
+		document.write("<button class = 'button' onclick = 'print()'><p>Print</p></button");
+		document.write("</body>");
+		document.write("</html>");
+	}
+}
+
+codes.new("GKWCC-GWEIY");
+
+function code ()
+{
+	c = document.getElementById("code_input");
+
+	if (codes.has(c.value) == true && 1000 * 60 * 5 + d < Date.now())
+	{
+		alert("Thank you!")
+
+		document.write("<!DOCTYPE HTML>");
+		document.write("<html>");
+		document.write("<head>");
+		document.write("</head>");
+		document.write("<body>");
+		document.write("<p>Please print out this page for Habib Khan to approve.</p>");
+		document.write("<button class = 'button' onclick = 'print()'><p>Print</p></button");
+		document.write("</body>");
+		document.write("</html>");
+	}
+}
+
+codes.new("DGSHV-TILWU");
+
+function code ()
+{
+	c = document.getElementById("code_input");
+
+	if (codes.has(c.value) == true && 1000 * 60 * 5 + d < Date.now())
+	{
+		alert("Thank you!")
+
+		document.write("<!DOCTYPE HTML>");
+		document.write("<html>");
+		document.write("<head>");
+		document.write("</head>");
+		document.write("<body>");
+		document.write("<p>Please print out this page for Habib Khan to approve.</p>");
+		document.write("<button class = 'button' onclick = 'print()'><p>Print</p></button");
+		document.write("</body>");
+		document.write("</html>");
+	}
+}
+
+codes.new("XRIQX-CFVDE");
+
+function code ()
+{
+	c = document.getElementById("code_input");
+
+	if (codes.has(c.value) == true && 1000 * 60 * 5 + d < Date.now())
+	{
+		alert("Thank you!")
+
+		document.write("<!DOCTYPE HTML>");
+		document.write("<html>");
+		document.write("<head>");
+		document.write("</head>");
+		document.write("<body>");
+		document.write("<p>Please print out this page for Habib Khan to approve.</p>");
+		document.write("<button class = 'button' onclick = 'print()'><p>Print</p></button");
+		document.write("</body>");
+		document.write("</html>");
+	}
+}
+
+codes.new("PTBJZ-VCOXW");
+
+function code ()
+{
+	c = document.getElementById("code_input");
+
+	if (codes.has(c.value) == true && 1000 * 60 * 5 + d < Date.now())
+	{
+		alert("Thank you!")
+
+		document.write("<!DOCTYPE HTML>");
+		document.write("<html>");
+		document.write("<head>");
+		document.write("</head>");
+		document.write("<body>");
+		document.write("<p>Please print out this page for Habib Khan to approve.</p>");
+		document.write("<button class = 'button' onclick = 'print()'><p>Print</p></button");
+		document.write("</body>");
+		document.write("</html>");
+	}
+}
+
+codes.new("SPIYC-KKGZE");
+
+function code ()
+{
+	c = document.getElementById("code_input");
+
+	if (codes.has(c.value) == true && 1000 * 60 * 5 + d < Date.now())
+	{
+		alert("Thank you!")
+
+		document.write("<!DOCTYPE HTML>");
+		document.write("<html>");
+		document.write("<head>");
+		document.write("</head>");
+		document.write("<body>");
+		document.write("<p>Please print out this page for Habib Khan to approve.</p>");
+		document.write("<button class = 'button' onclick = 'print()'><p>Print</p></button");
+		document.write("</body>");
+		document.write("</html>");
+	}
+}
+
+codes.new("MODKL-TRJGO");
+
+function code ()
+{
+	c = document.getElementById("code_input");
+
+	if (codes.has(c.value) == true && 1000 * 60 * 5 + d < Date.now())
+	{
+		alert("Thank you!")
+
+		document.write("<!DOCTYPE HTML>");
+		document.write("<html>");
+		document.write("<head>");
+		document.write("</head>");
+		document.write("<body>");
+		document.write("<p>Please print out this page for Habib Khan to approve.</p>");
+		document.write("<button class = 'button' onclick = 'print()'><p>Print</p></button");
+		document.write("</body>");
+		document.write("</html>");
+	}
+}
+
+codes.new("KSROL-OUNEQ");
+
+function code ()
+{
+	c = document.getElementById("code_input");
+
+	if (codes.has(c.value) == true && 1000 * 60 * 5 + d < Date.now())
+	{
+		alert("Thank you!")
+
+		document.write("<!DOCTYPE HTML>");
+		document.write("<html>");
+		document.write("<head>");
+		document.write("</head>");
+		document.write("<body>");
+		document.write("<p>Please print out this page for Habib Khan to approve.</p>");
+		document.write("<button class = 'button' onclick = 'print()'><p>Print</p></button");
+		document.write("</body>");
+		document.write("</html>");
+	}
+}
+
+codes.new("BRZEX-MVWQN");
+
+function code ()
+{
+	c = document.getElementById("code_input");
+
+	if (codes.has(c.value) == true && 1000 * 60 * 5 + d < Date.now())
+	{
+		alert("Thank you!")
+
+		document.write("<!DOCTYPE HTML>");
+		document.write("<html>");
+		document.write("<head>");
+		document.write("</head>");
+		document.write("<body>");
+		document.write("<p>Please print out this page for Habib Khan to approve.</p>");
+		document.write("<button class = 'button' onclick = 'print()'><p>Print</p></button");
+		document.write("</body>");
+		document.write("</html>");
+	}
+}
