fd7d665e6714aea0b4fd8e4fa87375c92f7a228a
HabibKhan1 <linuxforlife1@outlook.com>
2022-06-21 18:37:21 -0500

    Update code.js

--- survey/code.js
+++ survey/code.js
@@ -1,34 +1,7 @@
 codes = new Set();
 var d = Date.now();
 
-codes.add("BWXWC-XUGGZ");
-
-function code ()
-{
-	c = document.getElementById("code_input");
-
-	if (1000 * 60 * 5 + d < Date.now()) alert("Time's up! Code has expired.");
-	if (codes.has(c.value) == false) alert("Invalid code!");
-	if (codes.has(c.value) == true && 1000 * 60 * 5 + d > Date.now())
-	{
-		alert("Thank you!");
-
-		document.write("<!DOCTYPE HTML>");
-		document.write("<html>");
-		document.write("<head>");
-		document.write("<meta charset = 'utf8' name = 'viewport' content = 'width = device-width, initial-scale = 1.0'>");
-		document.write("<title>Take the survey!</title>");
-		document.write("<link rel = 'stylesheet' href = '/styles.css'>");
-		document.write("</head>");
-		document.write("<body>");
-		document.write("<h1>Please print out this page for Habib Khan to approve.</h1>");
-		document.write("<button class = 'button' onclick = 'print()'><p>Print</p></button");
-		document.write("</body>");
-		document.write("</html>");
-	}
-}
-
-codes.add("FYNCH-RGUSN");
+codes.add("CNJHC-FRJHJ");
 
 function code ()
 {
